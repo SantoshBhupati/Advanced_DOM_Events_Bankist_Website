295a28f0a2ab8a395a68790052f78219a6693201
SantoshBhupati <bhupatis20@gmail.com>
2021-12-16 10:46:17 +0530

    Creating Deleting And Inserting Element

--- script.js
+++ script.js
@@ -32,3 +32,33 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+//Selecting,Creating,And Deleting Elements
+
+ console.log(document.documentElement); //Selecting Whole Page
+ console.log(document.head); //To Select Head Of The Page
+ console.log(document.body); //To select Page Of The Body
+ const header =document.querySelector('.header') // TO Select Specific Class Of Document
+ 
+ const allSelections = document.querySelectorAll('.section'); //To Select all classes present in the document and its NodeList
+ console.log(allSelections);
+ 
+ document.getElementById('section--1');
+ const allbutton = document.getElementsByTagName('button'); // returns An HTML Collection 
+ console.log(allbutton);
+
+ //Creating And Deleting Elements
+ const message = document.createElement('div');
+ message.classList.add('cookie-message');
+ message.innerHTML =  'We use Cokkies for improved Functionality And Analaytics. <button class ="btn btn--close-cookie">Got it!</button>';
+//  header.prepend(message);
+ header.append(message);
+// header.append(message.cloneNode
+  // (true));
+
+
+  //deleting elements
+  document.querySelector('.btn--close-cookie').addEventListener('click',function(){
+    message.remove();
+  })
