dd8ccd1fc9879c6c228c60c62998a1d29e8b0bed
SantoshBhupati <bhupatis20@gmail.com>
2021-12-21 10:41:33 +0530

    implemenating Page Navigation

--- script.js
+++ script.js
@@ -34,54 +34,7 @@ document.addEventListener('keydown', function (e) {
 });
 
 
-//Selecting,Creating,And Deleting Elements
-
- console.log(document.documentElement); //Selecting Whole Page
- console.log(document.head); //To Select Head Of The Page
- console.log(document.body); //To select Page Of The Body
- const header =document.querySelector('.header') // TO Select Specific Class Of Document
- 
- const allSelections = document.querySelectorAll('.section'); //To Select all classes present in the document and its NodeList
- console.log(allSelections);
- 
- document.getElementById('section--1');
- const allbutton = document.getElementsByTagName('button'); // returns An HTML Collection 
- console.log(allbutton);
-
- //Creating And Deleting Elements
- const message = document.createElement('div');
- message.classList.add('cookie-message');
- message.innerHTML =  'We use Cokkies for improved Functionality And Analaytics. <button class ="btn btn--close-cookie">Got it!</button>';
-//  header.prepend(message);
- header.append(message);
-// header.append(message.cloneNode
-  // (true));
-
-
-  //deleting elements
-  document.querySelector('.btn--close-cookie').addEventListener('click',function(){
-    message.remove();
-  })
-
-  //STYLES ATTRIBUTES AND CLASSES
-  message.style.backgroundColor = '#37383d';
-
-  message.style.width = '100%'; //set as inline css
-  console.log(message.style.color) // prints nothing  because it returns only inline css elements
-  console.log(message.style.width); // 100%
-
-  // How to get external css
-  console.log(getComputedStyle(message).color);
-  console.log(getComputedStyle(message).height);
-
-  message.style.height =Number.parseFloat(getComputedStyle(message).height,10) + 40 + 'px';
-
-  //ATTRIUBUTES
-
-  const logo =  document.querySelector('.nav__logo');
-  console.log(logo.src);
-  console.log(logo.className);
-  console.log(logo.alt);
+//Btn Scrolling
 
   //SMOOTH SCROLLING
   const btnScrollTo = document.querySelector('.btn--scroll-to');
@@ -122,6 +75,88 @@ document.addEventListener('keydown', function (e) {
   //Modern Way TO Scrolling
    section_1.scrollIntoView({behavior:'smooth'});
  })
+///////////////////////////////////////////////////////////////////
+////////////////////////////////////
+//////////
+//PAGE NAVIGATION
+// document.querySelectorAll('.nav__link').forEach((el)=>{
+//   el.addEventListener('click',function(e){
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+//   })
+// })
+
+
+
+//Add EventListener To Common Parent Element
+document.querySelector('.nav__links').addEventListener('click',function(e){
+  e.preventDefault();
+  // console.log(e.target);
+  //Matching Element
+  if(e.target.classList.contains('nav__link')){
+    // console.log('Hi...');
+    const id = e.target.getAttribute('href');
+    // console.log(id);
+     document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+  }
+})
+
+
+
+
+///////////////////////////////////////////////////////////////////
+////////////////////////////////////
+/////////
+//Selecting,Creating,And Deleting Elements
+
+//  console.log(document.documentElement); //Selecting Whole Page
+//  console.log(document.head); //To Select Head Of The Page
+//  console.log(document.body); //To select Page Of The Body
+//  const header =document.querySelector('.header') // TO Select Specific Class Of Document
+ 
+//  const allSelections = document.querySelectorAll('.section'); //To Select all classes present in the document and its NodeList
+//  console.log(allSelections);
+ 
+//  document.getElementById('section--1');
+//  const allbutton = document.getElementsByTagName('button'); // returns An HTML Collection 
+//  console.log(allbutton);
+
+//  //Creating And Deleting Elements
+//  const message = document.createElement('div');
+//  message.classList.add('cookie-message');
+//  message.innerHTML =  'We use Cokkies for improved Functionality And Analaytics. <button class ="btn btn--close-cookie">Got it!</button>';
+// //  header.prepend(message);
+//  header.append(message);
+// // header.append(message.cloneNode
+//   // (true));
+
+
+//   //deleting elements
+//   document.querySelector('.btn--close-cookie').addEventListener('click',function(){
+//     message.remove();
+//   })
+
+//   //STYLES ATTRIBUTES AND CLASSES
+//   message.style.backgroundColor = '#37383d';
+
+//   message.style.width = '100%'; //set as inline css
+//   console.log(message.style.color) // prints nothing  because it returns only inline css elements
+//   console.log(message.style.width); // 100%
+
+//   // How to get external css
+//   console.log(getComputedStyle(message).color);
+//   console.log(getComputedStyle(message).height);
+
+//   message.style.height =Number.parseFloat(getComputedStyle(message).height,10) + 40 + 'px';
+
+//   //ATTRIUBUTES
+
+//   const logo =  document.querySelector('.nav__logo');
+//   console.log(logo.src);
+//   console.log(logo.className);
+//   console.log(logo.alt);
+
   
 
 //  const h1 = document.querySelector('h1');
